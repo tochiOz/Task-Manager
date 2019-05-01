57af2505229ee1c82dfc1ddd5ed1b5b43b78c06c
Tueloper <ozurumbatochukwu@yahoo.com>
2019-05-01 21:44:47 +0100

    Learning Mongoose

---
+++ src/db/mongoose.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-db', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+
+//here we define the mongoose models of the data to be inputted to the db
+//it set types like string, binary, array, number, boolean and so on.
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         // validate(value) {
+//         //     if(value.toLowerCase.includes(name)) {
+//         //         throw new Error('This is not a valid name')
+//         //     }
+//         // }
+//     },
+
+//     age: {
+//         type: Number
+//     }
+// })
+
+const User = mongoose.model('task', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+
+    password: {
+        type: String,
+        default: 1234567890,
+        minLength: 4,
+        lowercase: true,
+        validate(value) {
+            if (value === 'password') {
+                throw new Error('Please check your password and try again')
+            }
+        }
+    }
+})
+
+const nameQuery = new User({
+    email: ' EFHJ@NOIN.VM  ',
+    password: 'hoiuupiolknkYUVILHIl'
+})
+
+nameQuery.save().then(() => {
+    console.log(nameQuery)
+}).catch((err) => {
+    console.log(err)
+})
